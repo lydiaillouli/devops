52191ab6b8d643d057e128d03f9c14b58d340225
lydiaillouli <44178364+lydiaillouli@users.noreply.github.com>
2020-04-14 21:03:10 +0200

    Update users.js

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -9,23 +9,35 @@ module.exports = {
     if(!user.username)
       return callback(new Error("Wrong user parameters"), null)
 
+    if (Lookfor(user.username) == null){
     // Define the file for storing users information
-    config = configure()
-    const usersFile = path.join(config.users.db_dir, 'users')
+    //config = configure()
+    //const usersFile = path.join(config.users.db_dir, 'users')
 
     // Create user string from the json object
     // const strUser = user.username + ':' + user.firstname + ':' + user.lastname + os.EOL
     const strUser = `${user.username}:${user.firstname}:${user.lastname}${os.EOL}`
-
-    fs.appendFile(usersFile, strUser, (err) => {
+    
+      fs.appendFile(usersFile, strUser, (err) => {
       if (err) throw err
       callback(null, user.username)
     })
+    } else {
+      return callback(new Error("User Already Exists"), null)
+    }
   },
+  
   get: (username, callback) => {
     // TODO create this method
     // 1. Read the file
     // 2. Check every line in the loop ("for")
     // 3. Return an onject with user information, or null
+    let userObj = Lookfor(username)
+    if (userObj == null) {
+      return callback(new Error(" User not found"), null)
+    } else {
+      callback(null, userObj)
+    }
   }
 }
+exports.Lookfor = Lookfor;
