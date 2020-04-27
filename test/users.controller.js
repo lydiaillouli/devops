261a8caee5c4a372001eafc6be53d116baf39736
lydiaillouli <44178364+lydiaillouli@users.noreply.github.com>
2020-04-27 14:32:23 +0200

    Update users.controller.js

--- test/users.controller.js
+++ test/users.controller.js
@@ -1,58 +1,35 @@
-const { expect } = require('chai')
-const users = require('../src/controllers/users')
 const path = require('path')
 const fs = require('fs')
-const configure = require('../src/configure')
+const os = require('os')
 
-describe('Users', () => {
+configure = require('../configure')
+conig = configure()
 
-  beforeEach((done) => {
-    config = configure()
-    fs.writeFile(path.join(config.users.db_dir, 'users'), '', () => {
-      done()
+
+module.exports = {
+  create: (user, callback) => {
+    if (!user.username) return callback(new Error('Wrong user parameter'), null)
+
+    // TODO heck if user already exists
+    strUser = `${user.username}:${user.firstname}:${user.lastname}${os.EOL}`
+    fs.appendFile(path.join(conig.users.db_dir, 'users'), strUser, (err) => {
+      if (err) throw err
+      callback(null, user.username)
     })
-  })
-
-  describe('Create', ()=> {
-
-    it('create a new user', (done)=> {
-      const user = {
-        username: 'sergkudinov',
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
+  },
+  get: (username, callback) => {
+    // TODO create this method
+    console.log(username);
+    fs.readFile(path.join(conig.users.db_dir, 'users'), (err, data) => {
+      let users = data.toString();
+      let allusers = users.split("\n");
+      for (user of allusers) {
+        let myUser= user.split(":")
+        if (myUser[0] === username) {
+          //callback(null, myUser)
+        }
       }
-      users.create(user, (err, result) => {
-        expect(err).to.be.equal(null)
-        expect(result).to.be.equal('sergkudinov')
-        done()
-      })
+      if (err) throw err
     })
-
-    it('passing wrong user parameters', (done)=> {
-      const user = {
-        firstname: 'Sergei',
-        lastname: 'Kudinov'
-      }
-      users.create(user, (err, result) => {
-        expect(err).to.not.be.equal(null)
-        expect(result).to.be.equal(null)
-        done()
-      })
-    })
-
-    it('avoid creating an existing user', (done)=> {
-      // TODO create this test
-      // Warning: the user already exists
-      done()
-    })
-  })
-
-  describe('Get', ()=> {
-    // TODO Create test for the get method
-    it('get a user by username', (done) => {
-      // 1. Create a user
-      // 2. Check the result of the get method is correct
-      done()
-    })
-  })
-})
+  }
+}
